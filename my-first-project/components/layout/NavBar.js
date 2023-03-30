1a0e2ee7b4ad91088d3d4ef4f4798912bdaab2bf
bmwiner <nando.leiloes@gmail.com>
2023-03-30 22:32:40 +0100

    aula4

---
+++ my-first-project/components/layout/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="layout-navbar">
+      <p>Sobre o curso</p>
+      <p>Horários</p>
+      <p>Onde estamos</p>
+    </div>
+  );
+}
+
+export default NavBar;
--- my-first-project/pages/index.tsx
+++ my-first-project/pages/index.tsx
@@ -1,20 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import BasicCard from '../components/BasicCard'
+import Layout from '../components/layout/'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Meu primeiro  projecto react!
-          </p>
-        </div>
-        <BasicCard></BasicCard>
-      </main>
+<>
+<Layout/>
+</>
   )
 }
+
