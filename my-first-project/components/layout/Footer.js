f32bf3cbf90228284d9287cab27fc0f4d77669f6
bmwiner <nando.leiloes@gmail.com>
2023-04-06 22:44:09 +0100

    aula6

--- my-first-project/components/layout/Footer.js
+++ my-first-project/components/layout/Footer.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-  return (
-    <div className='layout_footer'>Footer</div>
-  )
+  return <div className="layout_footer">Footer do projeto</div>;
 }
 
-export default Footer
+export default Footer;
